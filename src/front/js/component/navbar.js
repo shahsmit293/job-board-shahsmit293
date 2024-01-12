396d87723b27fac4febc192b8df42bdc798a63c0
shahsmit293 <138520584+shahsmit293@users.noreply.github.com>
2024-01-12 09:42:11 +0000

    test ci/cd again

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -21,7 +21,7 @@ export const Navbar = () => {
             }
           }}
         >
-          Logo
+          Logoioioio
         </button>
         <button
           className="navbar-toggler"
