14901eba061bb2c21c10a5ed762fe68701a2e2da
shahsmit293 <138520584+shahsmit293@users.noreply.github.com>
2024-02-24 23:11:58 +0000

    aws region corrected  for s3 bucket

--- src/api/routes.py
+++ src/api/routes.py
@@ -129,7 +129,7 @@ def add_job():
     session = boto3.Session(
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-        region_name='us-east-2'
+        region_name='us-east-1'
     )
     s3 = session.client('s3')
     s3.upload_fileobj(company_logo, 'hiremasterylogo', company_logo.filename)
